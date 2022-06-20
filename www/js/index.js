2e1a7d692bffdda0d5a711adbc1404637dd61e5b
Vlad059 <107025391+Vlad059@users.noreply.github.com>
2022-06-20 16:12:26 +0500

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    try{       
+        function error() {
+          console.log('some error');
+        }
+        
+        function success( status ) {
+          if( !status.hasPermission ) console.log('has not permission');
+          else console.log('has permission');
+        }
+
+        let permissions = cordova.plugins.permissions;
+    
+        permissions.requestPermission(permissions.CAMERA, success, error);
+
+        // srcs=['/js/chunk-vendors.200fa63e.js','/js/app.bc6c42ce.js'].forEach(element => {
+        //     let script = document.createElement('script');
+        //     script.src = element;
+        //     document.head.append(script);
+        // })
+    }
+    catch (err){
+        alert(err);
+    }
+}
